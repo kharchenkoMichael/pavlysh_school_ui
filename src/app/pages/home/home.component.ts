ab0b4e2c0ecf373d8120f8929e0b6a521e0ce3e2
kharchenkoMichael <kharchenko.michael.a@gmail.com>
2025-03-29 23:06:15 +0200

    Add home page structure and add header component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CarouselComponent } from '../../shared/components/carousel/carousel.component';
+import { NewsComponent } from '../../shared/components/news/news.component';
+import { AdmissionComponent } from '../../shared/components/admission/admission.component';
+import { TestimonialsComponent } from '../../shared/components/testimonials/testimonials.component';
+import { AddressComponent } from '../../shared/components/address/address.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    CarouselComponent,
+    NewsComponent,
+    AdmissionComponent,
+    TestimonialsComponent,
+    AddressComponent,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
