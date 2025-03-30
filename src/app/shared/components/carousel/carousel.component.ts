ce862f364377d7ebc2a69ef3e7c9e88be6dce557
kharchenkoMichael <kharchenko.michael.a@gmail.com>
2025-03-30 18:32:37 +0300

    Add carusel

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss'],
+  imports: [CommonModule],
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+  images = [
+    'assets/images/carusel1.jpg',
+    'assets/images/carusel2.jpg',
+    'assets/images/carusel3.jpg',
+    'assets/images/carusel4.jpg',
+  ];
+  currentIndex = 0;
+  currentTranslate = 0;
+  interval: any;
 
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.interval = setInterval(() => {
+      this.next();
+    }, 3000);
+  }
+
+  next() {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.updateTranslate();
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.images.length - 1;
+    }
+    this.updateTranslate();
+  }
+
+  updateTranslate() {
+    this.currentTranslate = -this.currentIndex * window.innerWidth;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
