46f72b242fe81122808046b5433af059744c3121
kharchenkoMichael <kharchenko.michael.a@gmail.com>
2025-03-30 16:16:31 +0300

    Add headers menu and mobile version

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -12,4 +12,14 @@ export class HeaderComponent {
   toggleMobileMenu() {
     this.mobileMenuActive = !this.mobileMenuActive;
   }
+
+  toggleSubmenu(event: Event) {
+    const submenus = document.querySelectorAll('.mobile-submenu');
+    submenus.forEach((submenu) => submenu.classList.remove('active'));
+
+    const submenu = (event.target as HTMLElement).nextElementSibling;
+    if (submenu) {
+      submenu.classList.toggle('active');
+    }
+  }
 }
